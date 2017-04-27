f96c91cee9ad8573b69783e58ec1192e6b83afb3
Fachrul Pralienka Bani Muhamad <Fachrul Pralienka Bani Muhamad>
2017-04-28 00:36:28 +0700

    Penyederhanaan variabel

--- CM1.m
+++ CM1.m
@@ -7,108 +7,84 @@ DatasetCM1 = csvread('CM1.csv');
 %load file CSV dataset (remove duplicate)
 CM1Unique = csvread('CM1Unique.csv');
 
-for IterasiFiturFold = 1:21                    
-    % FOLD1(1:45) (46:89) (90:133) (134:177) (178:221) (222:265) (266:309) (310:353) (354:397) (398:442)
-    Fold1Fitur = strcat('Fold1Fitur',num2str(IterasiFiturFold)); 
-    Fold1.(Fold1Fitur) = [CM1Unique(1:45,IterasiFiturFold)  CM1Unique(1:45,22)]; %Matrix fitur 1 terhadap kelas
-    Fold1.(Fold1Fitur) = sortrows(Fold1.(Fold1Fitur));                                       
-end
-clear Fold1Fitur IterasiFiturFold;  
-
 % Pembagian fold = 10
 k = 10;
 vektorCM1 = CM1Unique(:,1);
 cvFolds = crossvalind('Kfold', vektorCM1, k);
 clear vektorCM1;
 
+% Array 10 baris, 21 kolom
+M = cell( 10 , (size(CM1Unique,2)-1) ); %(jumlah fold, jumlah fitur)
+
 % Iterasi fold
-for i = 1:k    
+for iFold = 1:k    
     
     % Pembagian data training dan testing per setiap fold
-    testIdx  = (cvFolds == i);                
-    trainIdx(:,i) = ~testIdx;    
-        
-    
-    jumlahTraining(:,i) = 0;
-    jumlahTrue(:,i) = 0;
-    jumlahFalse(:,i) = 0;
-    piTrue(:,i) = 0;
-    piFalse(:,i) = 0;
-    % Menghitung jumlah training 
-    for iterasi = 1 : length(CM1Unique)
-        for iterasi2 = 1 : size(CM1Unique,2)                            
-            if trainIdx(iterasi,i) == 1
-                jumlahTraining(iterasi2,i) = jumlahTraining(iterasi2,i) + 1;
-                if CM1Unique(iterasi,22) == 1
-                    jumlahTrue(iterasi2,i) = jumlahTrue(iterasi2,i) + 1;
-                else
-                    jumlahFalse(iterasi2,i) = jumlahFalse(iterasi2,i) + 1;
-                end
-            end           
+    testIdx  = (cvFolds == iFold);                
+    trainIdx(:,iFold) = ~testIdx;    
+            
+    % Menghitung jumlah training, testing, kelas true, dan kelas false
+    jmlTraining(iFold,1) = 0;
+    jmlTesting(iFold,1) = 0;
+    jmlTrue(iFold,1) = 0;
+    jmlFalse(iFold,1) = 0;     
+    for iBarisCM1Unique = 1 : length(CM1Unique) % Looping 1 - 442
+        if trainIdx(iBarisCM1Unique,iFold) == 1 % Mengambil urutan (trainIdx = 1) dengan CM1Unique
+            jmlTraining(iFold,1) = jmlTraining(iFold,1) + 1; % Increment jmlTraining jika trainIdx nya 1
+            if CM1Unique(iBarisCM1Unique,22) == 1 % Berapa kelas true dan false dari jmlTraining
+                jmlTrue(iFold,1) = jmlTrue(iFold,1) + 1; % Increment jmlTrue jika kelas = 1
+            else
+                jmlFalse(iFold,1) = jmlFalse(iFold,1) + 1; % Increment jmlFalse jika kelas = 0
+            end
+        else
+            jmlTesting(iFold,1) = jmlTesting(iFold,1) + 1; % Increment jmlTesting jika trainIdx nya ~= 1
         end
-    end        
-    
-    piTrue(1,i) = jumlahTrue(1,i)/jumlahTraining(1,i);
-    piFalse(1,i) = jumlahFalse(1,i)/jumlahTraining(1,i);
-    Log2piTrue(1,i) = log2(piTrue(1,i));
-    Log2piFalse(1,i) = log2(piFalse(1,i));
-    kaliLogTrue(1,i) = Log2piTrue(1,i) * piTrue(1,i);
-    kaliLogFalse(1,i) = Log2piFalse(1,i) * piFalse(1,i);
-    entropyParent(1,i) = abs( kaliLogTrue(1,i) + kaliLogFalse(1,i));
+    end          
+    clear iBarisCM1Unique;
+        
+    % Rumus menghitung entropy parent di tahap EBD dari setiap fold            
+    piTrue(iFold,1) = jmlTrue(iFold,1)/jmlTraining(iFold,1);
+    piFalse(iFold,1) = jmlFalse(iFold,1)/jmlTraining(iFold,1);
+    Log2piTrue(iFold,1) = log2(piTrue(iFold,1));
+    Log2piFalse(iFold,1) = log2(piFalse(iFold,1));
+    kaliLogTrue(iFold,1) = Log2piTrue(iFold,1) * piTrue(iFold,1);
+    kaliLogFalse(iFold,1) = Log2piFalse(iFold,1) * piFalse(iFold,1);
+    entropyParent(iFold,1) = abs( kaliLogTrue(iFold,1) + kaliLogFalse(iFold,1) );    
     clear piTrue piFalse Log2piTrue Log2piFalse kaliLogFalse kaliLogTrue;
     
+    % Menyederhanakan variable (diringkas menjadi satu metrik)
+    keteranganCM1(iFold,:) = [jmlTesting(iFold,:) jmlTraining(iFold,:) jmlTrue(iFold,:) jmlFalse(iFold,:) entropyParent(iFold,:)];
+    clear jmlTesting jmlTraining jmlTrue jmlFalse entropyParent;;
     
+    % Looping array M dari 1 hingga 21(semua fitur kecuali kelas)
+    for z = 1 : size(CM1Unique,2)-1
+        % Looping untuk mengisi data dummy berdasarkan jumlah training dari masing-masing fold
+        M{iFold,z} = zeros( keteranganCM1(iFold,2) , 2);        		
+    end
+    clear z iJmlTraining;
     
-%     for iterasi = 1 : size(jumlahTraining,2)
-%         M = cell( jumlahTraining(1,iterasi) , k ) ; %FOLD
-%         
-%         
-%     end
-    
-%     for iterasi = 1 : k  %iterasi FOLD
-%         for iterasi2 = 1 : length(trainIdx) %iterasi baris
-%             %M{iterasi2,k} = zeros(442,2);		
-%             if trainIdx(iterasi2,i) == 1 %cek training yang nilainya 1
-%                 for iterasi3 = 1 : jumlahTraining(1,i) %
-%                     for iterasi4 = 1: size(CM1Unique,2) %iterasi kolom
-%                         for iterasi5 = 1:2                            
-%                             M{iterasi3,k}(iterasi3,iterasi5) = 0;%[CM1Unique(iterasi3,iterasi4) CM1Unique(iterasi3 , 22)];
-%                         end                                                
-%                     end                
-%                 end                                    
-%             end
-%         end
-%     end
-    %M{1,1}(1,1)=44;
-    
-    
-    
-    
-%     for iterasi = 1 : size(trainIdx)
-%         M = cell(i,:);
-%         for k = 1 : i
-%             M{k,i} = [CM1Unique(:,1) CM1Unique(:,22)]
-%     end
-    
-%     for iterasi = 1 : length(trainIdx)
-%         if trainIdx(iterasi,i) == 1
-%             for iterasi2 = 1 : jumlahTraining(i)-1
-% %                 fiturKelas = cell( jumlahTraining(i),i );
-%                 for iterasi3 = 1 : 22
-%                     %sortFitur(iterasi2,i) = cell[CM1Unique(iterasi,iterasi3) CM1Unique(iterasi,22)];
-% %                     fiturKelas{iterasi3} = [CM1Unique(iterasi,iterasi3) CM1Unique(iterasi,22)];
+    % Mengganti data dummy pada array M dengan data training asli
+%     for iBarisCell = 1 : 21 %Fitur
+%        for iKolomCell = 1 : 10 %Fold
+%            for iBarisMetrik = 1 : 442 %Baris Data
+%                 for iKolomMetrik = 1 : 2         
+%                     for iCocokFitur = 1 : 21
+%                         if iBarisCell == iCocokFitur
+%                             M{iBarisCell,iKolomCell}(iBarisMetrik,iKolomMetrik) = CM1Unique(iBarisMetrik,iBarisCell);
+%                         end
+%                         if iKolomMetrik == 2
+%                             M{iBarisCell,iKolomCell}(iBarisMetrik,iKolomMetrik) = CM1Unique(iBarisMetrik,22);
+%                         end
+%                     end                                                
 %                 end
-%                 
-%             end
-%                                     
-%         end        
+%            end
+%        end
 %     end
-    
     
     
 end
   
-clear i cvFolds iterasi k testIdx;
+clear iFold cvFolds iterasi k testIdx;
 
 
 
